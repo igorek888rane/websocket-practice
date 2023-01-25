4a3f672bfa89768d061db5c9a665408f62c2c15f
igor <igorek888rane@gmail.com>
2023-01-25 08:59:29 +0300

    webSocketServer

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import {WebSocketServer} from 'ws';
+
+
+const wss = new WebSocketServer({
+    port: 5000
+}, () => console.log('Server started on 5000'))
+
+wss.on('open', function connection(ws) {
+    ws.on('message', (message) => {
+        message = JSON.parse(message)
+        switch (message.event) {
+            case 'message':
+                broadcastMessage(message)
+                break
+            case 'connection':
+                broadcastMessage(message)
+                break
+        }
+    })
+})
+const message = {
+    event: 'message/connection',
+    id: 123,
+    date: '25.01.2023',
+    username: 'Igor',
+    message: 'Привет'
+}
+
+function broadcastMessage(message) {
+    wss.clients.forEach(client => {
+        client.send(JSON.stringify(message))
+    })
+}
