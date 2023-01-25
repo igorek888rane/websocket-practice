d2a82a75b26a2dc9e3dc67e88f917044f66f4471
igor <igorek888rane@gmail.com>
2023-01-25 09:38:29 +0300

    webSocketServer

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const wss = new WebSocketServer({
     port: 5000
 }, () => console.log('Server started on 5000'))
 
-wss.on('open', function connection(ws) {
+wss.on('connection', function connection(ws) {
     ws.on('message', (message) => {
         message = JSON.parse(message)
         switch (message.event) {
@@ -13,18 +13,19 @@ wss.on('open', function connection(ws) {
                 broadcastMessage(message)
                 break
             case 'connection':
+                console.log('s')
                 broadcastMessage(message)
                 break
         }
     })
 })
-const message = {
-    event: 'message/connection',
-    id: 123,
-    date: '25.01.2023',
-    username: 'Igor',
-    message: 'Привет'
-}
+// const message = {
+//     event: 'message/connection',
+//     id: 123,
+//     date: '25.01.2023',
+//     username: 'Igor',
+//     message: 'Привет'
+// }
 
 function broadcastMessage(message) {
     wss.clients.forEach(client => {
